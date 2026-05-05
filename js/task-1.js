2c53f575a00af4cb58d864a9381fdce7d0266054
Denys Tryshkin <denystryshkin@gmail.com>
2026-05-04 20:00:13 -0400

    added task-1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesList.children.length}`);
+
+categoriesItems.forEach(item => {
+    console.log(`Category: ${item.children[0].textContent}`)
+    console.log(`Elements: ${item.children[1].children.length}`)
+});
